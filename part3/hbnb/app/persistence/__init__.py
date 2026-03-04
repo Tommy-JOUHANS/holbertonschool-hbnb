1fefa7bf7c40f0c962fc4e8a896149b1d5f91ba8
James0812 <james.roussel008@gmail.com>
2026-03-04 15:30:45 +0100

    add the code

--- part3/hbnb/app/persistence/__init__.py
+++ part3/hbnb/app/persistence/__init__.py
@@ -1,9 +1,26 @@
 """
-Persistence layer package.
-
-Provides access to repository implementations.
+Persistence layer initialization.
+Provides repository selection based on configuration.
 """
+import os
+from hbnb.app.persistence.repository import InMemoryRepository, SQLAlchemyRepository
 
-from hbnb.app.persistence.repository import InMemoryRepository
 
-__all__ = ["InMemoryRepository"]
+def get_repository(use_db=None):
+    """
+    Factory function to get the appropriate repository.
+    
+    Args:
+        use_db: If True, use SQLAlchemy. If False, use InMemory. 
+                If None, use environment variable.
+    
+    Returns:
+        Repository class (not instance)
+    """
+    if use_db is None:
+        use_db = os.getenv('USE_DATABASE', 'false').lower() == 'true'
+    
+    if use_db:
+        return SQLAlchemyRepository
+    else:
+        return InMemoryRepository
