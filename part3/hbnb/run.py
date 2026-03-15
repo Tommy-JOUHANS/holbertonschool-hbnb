4fa3ce704c47dcc24cc48bcee79aef0fe2b85b7c
Tommy JOUHANS <12119@holbertonstudents.com>
2026-03-15 22:03:41 +0100

    Passer une string (le plus simple)

--- part3/hbnb/run.py
+++ part3/hbnb/run.py
@@ -2,9 +2,9 @@
 Application entry point.
 """
 from hbnb.app import create_app
-from hbnb.app.config import DevelopmentConfig
-
-app = create_app(DevelopmentConfig)
-
+from hbnb.config import DevelopmentConfig
+ 
+app = create_app('development')
+ 
 if __name__ == '__main__':
     app.run(debug=True)
